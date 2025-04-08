117c0500c646cc15ca1ae408bf72f60da683083d
Philopateer Ghaly <philogeee@gmail.com>
2025-04-08 10:32:05 -0600

    compiling

--- src/compile_all.py
+++ src/compile_all.py
@@ -3,12 +3,13 @@ import subprocess
 import re
 from multiprocessing import Pool, cpu_count
 
-NUM_CORES=12
+NUM_CORES=8
 
 # Files starting with any of these will be skipped
 # exc_list = ['s18']  # Add more as needed
 # exc_list = ['s09', 's10', 's11', 's12', 's13', 's14', 's15', 's16']
-exc_list = ['s01', 's02', 's03', 's04', 's05', 's06', 's07', 's08', 's09',]
+# exc_list = ['s01', 's02', 's03', 's04', 's05', 's06', 's07', 's08', 's09',]
+exc_list = ['s20']
 
 # Pattern to match files like s01_..., s02_..., etc.
 pattern = re.compile(r'^(s\d{2}).*\.py$')
@@ -30,16 +31,20 @@ def filter_files(filename):
 # Command runner
 def compile_file(filename):
     if '_mgl_' in filename:
-        cmd = ['manimgl', filename, '-w', '-l']
+        # cmd = ['manimgl', filename, '-w', '-l']
+        cmd = ['manimgl', filename, '-w', '--uhd']
     else:
-        cmd = ['manim', '-ql', filename]
+        # cmd = ['manim', '-ql', filename]
+        cmd = ['manim', '-qk', filename]
     print(f"Running: {' '.join(cmd)}")
     subprocess.run(cmd)
 
 if __name__ == '__main__':
     filtered = list(filter(None, map(filter_files, files)))
-    with Pool(processes=NUM_CORES) as pool:
-        pool.map(compile_file, filtered)
+    for i in range(0, len(filtered), 4):
+        group = filtered[i:i + 4]
+        with Pool(processes=min(len(group), NUM_CORES)) as pool:
+            pool.map(compile_file, group)
 
 
 # import os
